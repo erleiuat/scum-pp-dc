a362e6655146f597397f3bf5010df73b763a964e
Elia <reutlinger.elia@gmail.com>
2021-05-31 18:31:57 +0200

    Update cmds.js

--- app/services/cmdhandler/cmds.js
+++ app/services/cmdhandler/cmds.js
@@ -96,7 +96,7 @@ exports.restart_countdown = async function restart_countdown(key, cmd) {
         countDownDate.setHours(0)
     }
 
-    let distance = countDownDate - now
+    let distance = countDownDate.getTime() - now.getTime()
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
     let seconds = Math.floor((distance % (1000 * 60)) / 1000)
