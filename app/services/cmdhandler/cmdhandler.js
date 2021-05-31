5f3f266e594320f1923d309f4e98c7b3ec5c44e8
Elia <reutlinger.elia@gmail.com>
2021-05-31 23:46:56 +0200

    aaa

--- app/services/cmdhandler/cmdhandler.js
+++ app/services/cmdhandler/cmdhandler.js
@@ -6,11 +6,12 @@ const cmds = require('./cmds')
 
 exports.start = async function start() {
     let i = 0
+    this.announce()
 
     do {
         await global.sleep.timer(0.01)
         if (Object.keys(global.commands).length < 1) continue
-        
+
         let newCmds = {}
         for (const e in global.commands) {
             let cmd = global.commands[e]
@@ -33,9 +34,10 @@ exports.start = async function start() {
 
 exports.sendCommands = async function sendCommands(key, cmdObj) {
 
+    console.log(cmdObj)
     console.log(sn + 'Sending Commands per FTP')
 
-    fs.writeFileSync('./app/storage/tmpCmd/cmds.json', JSON.stringify(cmdObj))
+    fs.writeFileSync('./app/storage/tmpCmd/' + key + '_cmds.json', JSON.stringify(cmdObj))
 
     try {
         await ftp.access({
@@ -46,12 +48,65 @@ exports.sendCommands = async function sendCommands(key, cmdObj) {
             secure: true
         })
 
-        await ftp.uploadFrom('./app/storage/tmpCmd/cmds.json', process.env.RM_CMD_FTP_DIR + key + '_cmds.json')
+        await ftp.uploadFrom('./app/storage/tmpCmd/' + key + '_cmds.json', process.env.RM_CMD_FTP_DIR + key + '_cmds.json')
 
     } catch (error) {
         throw new Error(error)
     }
 
     ftp.close()
+
+}
+
+exports.announce = async function announce() {
+
+    let announced = {
+        '17:55': {
+            text: '#Announce Restart in 5 Minutes!',
+            done: false
+        },
+        '17:59': {
+            text: '#Announce Restart in 1 Minute!',
+            done: false
+        },
+        '23:55': {
+            text: '#Announce Restart in 5 Minutes!',
+            done: false
+        },
+        '23:59': {
+            text: '#Announce Restart in 1 Minute!',
+            done: false
+        }
+    }
+
+    do {
+
+        await global.sleep.timer(5)
+
+        let now = new Date()
+        let time = now.getHours() + ':' + now.getMinutes()
+
+        if (announced[time]) {
+            if (announced[time].done) continue
+            let key = 'announce_' + now.getHours() + '_' + now.getMinutes()
+            let tmpObj = {}
+            tmpObj[key] = {
+                type: 'global',
+                commands: [
+                    '#SetFakeName [SF-BOT][RESTART]',
+                    announced[time].text,
+                    '#ClearFakeName'
+                ]
+            }
+            announced[time].done = true
+            await this.sendCommands(key, tmpObj)
+
+        } else {
+            for (const e in announced) {
+                announced[e].done = false
+            }
+        }
+
+    } while (true)
 
 }
