0875b691caefd04af3e6a1f1ff7eef098611b14d
Elia <reutlinger.elia@gmail.com>
2021-06-15 14:53:14 +0200

    Update start_game.py

--- app/cpscripts/start_game.py
+++ app/cpscripts/start_game.py
@@ -56,8 +56,8 @@ control.teleport('#Teleport -117114.336 -66718.719 37064.668')
 # -------------------------------   SETUP
 
 # light_up.doIt()
-# big_business.doIt(4)
-# make_break.doIt()
+big_business.doIt(4)
+make_break.doIt()
 
 # -------------------------------   SETUP DONE
 
