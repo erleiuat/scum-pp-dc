e3a6760ba1609a8747b0e81893ca0867b87873e5
Elia <reutlinger.elia@gmail.com>
2021-06-16 18:03:44 +0200

    Update start_game.py

--- app/cpscripts/start_game.py
+++ app/cpscripts/start_game.py
@@ -5,14 +5,14 @@ import webbrowser
 import subprocess
 import os
 
-fullBatPath = os.path.dirname(os.path.realpath(__file__)) + '\use'
+fullBatPath = os.path.dirname(os.path.realpath(__file__))
 
 
 print('\n\n')
 
 print(' -> Killing running processes')
-subprocess.call([fullBatPath + '\kill_steam.bat'])
-subprocess.call([fullBatPath + '\kill_scum.bat'])
+subprocess.call([fullBatPath + '\\use\\kill_steam.bat'])
+subprocess.call([fullBatPath + '\\use\\kill_scum.bat'])
 scb.sleep(5)
 
 print(' -> Starting Scum')
