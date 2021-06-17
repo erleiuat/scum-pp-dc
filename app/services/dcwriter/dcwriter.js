5513baa89b52122056e08f5152facb361efd1300
Elia <reutlinger.elia@gmail.com>
2021-06-17 13:38:00 +0200

    Update dcwriter.js

--- app/services/dcwriter/dcwriter.js
+++ app/services/dcwriter/dcwriter.js
@@ -115,9 +115,10 @@ async function sendAdmins(dcClient) {
 
         if (line.message.toLowerCase().includes('setfakename')) shouldHide = true
         else if (line.message.toLowerCase().includes('clearfakename')) shouldHide = true
+        else if (line.message.toLowerCase().includes('listplayers')) shouldHide = true
 
         if (!shouldHide) {
-            if (aList[line.steamID].alertCommands) await channel.send('** @everyone The following command was not executed by the bot but by <@' + aList[line.steamID].discord + '>.\nPlease explain by replying to the message what you needed the command for.**\n\n_If you don\'t want to receive admin abuse notifications in the future, change the notification settings of this channel to "nothing"_')
+            if (aList[line.steamID] && aList[line.steamID].alertCommands) await channel.send('** @everyone The following command was not executed by the bot but by <@' + aList[line.steamID].discord + '>.\nPlease explain by replying to the message what you needed the command for.**\n\n_If you don\'t want to receive admin abuse notifications in the future, change the notification settings of this channel to "nothing"_')
             await channel.send(new Discord.MessageEmbed(await format.admin(global.newEntries.admin[el])))
             console.log(sn + 'Admin sent: ' + el)
         }
