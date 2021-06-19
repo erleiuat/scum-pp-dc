9867fdf1bef5f1154689322a5582cd39be6b16ef
Elia <reutlinger.elia@gmail.com>
2021-06-19 12:37:13 +0200

    Update bot.js

--- app/bot.js
+++ app/bot.js
@@ -11,74 +11,64 @@ const cmdHandler = require('./services/cmdhandler/cmdhandler')
 const dcHandler = require('./services/dchandler')
 const dcBot = require('./services/dcbot')
 
+
 exports.start = async function start() {
+
+    /*
+        Possible args:
+        - dcBot     -> Discord functions for handling new Users etc.
+        - logBot    -> Functions for processing log-data to discord and commands
+        - inGame    -> Scum-Bot handling commands and more (only with -logBot enabled)
+    */
 
     let args = process.argv.slice(2).map(el => el.replace('-', '').trim())
     dcClient.on('ready', () => {
 
         console.log(sn + `Logged in as ${dcClient.user.tag}!`)
 
-        console.log(sn + 'Starting Discord-Handler functionalities')
-        dcHandler.start(dcClient)
+        if (args.includes('dcBot'))
+            startDCBot(dcClient)
 
-        console.log(sn + 'Starting Discord-Writer')
-        dcWriter.start(dcClient)
+        if (args.includes('logBot'))
+            startLogFunctions(dcClient)
 
-        console.log(sn + 'Starting State-Display')
-        state.start(dcClient)
-
-        console.log(sn + 'Starting Statistics')
-        statistics.start(dcClient)
-
-        console.log(sn + 'Starting FTP-Watcher')
-        ftpWatcher.start()
-
-        console.log(sn + 'Starting Log-Processor')
-        logProcessor.start()
-
-        console.log(sn + 'Starting Discord-Bot')
-        dcBot.start(dcClient)
-
-        /*
-        if (!args.includes('discord')) {
-
-            console.log(sn + 'Starting Discord-Handler functionalities')
-            dcHandler.start(dcClient)
-
-            console.log(sn + 'Starting Discord-Writer')
-            dcWriter.start(dcClient)
-
-            console.log(sn + 'Starting State-Display')
-            state.start(dcClient)
-
-            console.log(sn + 'Starting Statistics')
-            statistics.start(dcClient)
-
-            console.log(sn + 'Starting FTP-Watcher')
-            ftpWatcher.start()
-
-            console.log(sn + 'Starting Log-Processor')
-            logProcessor.start()
-
-            if (!args.includes('noIngame')) {
-                console.log(sn + 'Starting Command-handler')
-                cmdHandler.start()
-            }
-
-            if (args.includes('withdiscord')) {
-                console.log(sn + 'Starting Discord-Bot')
-                dcBot.start(dcClient)
-            }
-
-        } else {
-            console.log(sn + 'Starting Discord-Bot')
-            dcBot.start(dcClient)
-        }
-        */
+        if (args.includes('inGame'))
+            startIngameBot(dcClient)
 
     })
 
     console.log(sn + 'Login on Discord')
     dcClient.login(process.env.DISCORD_TOKEN)
 
+}
+
+
+async function startDCBot(dcClient) {
+    console.log(sn + 'Starting Discord-Bot')
+    dcBot.start(dcClient)
+}
+
+async function startLogFunctions(dcClient) {
+    console.log(sn + 'Starting Discord-Handler functionalities')
+    dcHandler.start(dcClient)
+
+    console.log(sn + 'Starting Discord-Writer')
+    dcWriter.start(dcClient)
+
+    console.log(sn + 'Starting State-Display')
+    state.start(dcClient)
+
+    console.log(sn + 'Starting Statistics')
+    statistics.start(dcClient)
+
+    console.log(sn + 'Starting FTP-Watcher')
+    ftpWatcher.start()
+
+    console.log(sn + 'Starting Log-Processor')
+    logProcessor.start()
+}
+
+async function startIngameBot(dcClient) {
+    console.log(sn + 'Starting Command-handler')
+    cmdHandler.start()
 }
