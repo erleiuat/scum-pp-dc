5c0b04f0a1f24b722f0c73249513d0b989639cb6
Elia <reutlinger.elia@gmail.com>
2021-06-19 18:04:46 +0200

    Update messages.js

--- app/services/cmdhandler/messages.js
+++ app/services/cmdhandler/messages.js
@@ -1,6 +1,22 @@
 exports.list = {
+    '6:55': {
+        text: '#Announce Restart in 5 Minutes!',
+        done: false
+    },
+    '6:57': {
+        text: 'RESTART-INFO: Don\'t drive and make sure there are no valuable items on the floor!',
+        done: false
+    },
+    '6:59': {
+        text: '#Announce Restart in 1 Minute!',
+        done: false
+    },
     '11:55': {
         text: '#Announce Restart in 5 Minutes!',
+        done: false
+    },
+    '11:57': {
+        text: 'RESTART-INFO: Don\'t drive and make sure there are no valuable items on the floor!',
         done: false
     },
     '11:59': {
@@ -11,12 +27,20 @@ exports.list = {
         text: '#Announce Restart in 5 Minutes!',
         done: false
     },
+    '17:57': {
+        text: 'RESTART-INFO: Don\'t drive and make sure there are no valuable items on the floor!',
+        done: false
+    },
     '17:59': {
         text: '#Announce Restart in 1 Minute!',
         done: false
     },
     '23:55': {
         text: '#Announce Restart in 5 Minutes!',
+        done: false
+    },
+    '23:57': {
+        text: 'RESTART-INFO: Don\'t drive and make sure there are no valuable items on the floor!',
         done: false
     },
     '23:59': {
