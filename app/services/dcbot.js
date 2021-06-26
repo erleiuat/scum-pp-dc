b93e8e5a196ef9ab7796b194648d6dac4a9bb6de
Elia <reutlinger.elia@gmail.com>
2021-06-26 17:32:55 +0200

    Update dcbot.js

--- app/services/dcbot.js
+++ app/services/dcbot.js
@@ -87,8 +87,8 @@ exports.start = async function start(dcClient) {
         else if (msg.content.toLowerCase().startsWith('!buildserver')) buildServer(dcClient, msg)
         else if (msg.channel.id == process.env.DISCORD_CH_LANGUAGE) setLang(msg)
         else if (msg.author.id !== process.env.DISCORD_BOT_ID) {
-            if (msg.content.toLowerCase().includes('i\'m')) imBot(msg.channel, msg.content.substring(msg.content.toLowerCase().indexOf('i\'m')))
-            if (msg.content.toLowerCase().includes('i am')) imBot(msg.channel, msg.content.substring(msg.content.toLowerCase().indexOf('i am')))
+            if (msg.content.toLowerCase().includes('i\'m')) imBot(msg.channel, msg.content.substring(msg.content.toLowerCase().indexOf('i\'m')+3))
+            if (msg.content.toLowerCase().includes('i am')) imBot(msg.channel, msg.content.substring(msg.content.toLowerCase().indexOf('i am')+4))
         }
     })
 
